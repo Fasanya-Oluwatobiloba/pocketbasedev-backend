5e3456d729c77fbdc050a047fd3548e3e6ce7284
Your Name <you@example.com>
2025-05-28 23:54:14 +0100

    added pocketbase.exe to run pocketbase locally and later delete it

---
+++ pb_migrations/1748249287_updated_sermons.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2988540424")
+
+  // update collection data
+  unmarshal({
+    "deleteRule": "@request.auth.collectionName = \"user\""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2988540424")
+
+  // update collection data
+  unmarshal({
+    "deleteRule": "@request.auth.collectionName = true"
+  }, collection)
+
+  return app.save(collection)
+})
